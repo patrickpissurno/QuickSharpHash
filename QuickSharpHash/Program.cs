827291f80281936d0be68008d337adb64325bf49
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-08-11 20:44:15 -0300

    First working version

---
+++ QuickSharpHash/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace QuickSharpHash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Usage: hash.exe <file1> [<file2> <file3> <...>]");
+                Environment.Exit(1);
+            }
+
+            foreach (var arg in args)
+                Console.WriteLine(ComputeFileHash(arg));
+
+            Console.WriteLine();
+            Console.WriteLine("Done.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static string ComputeFileHash(string filePath)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    return Convert.ToBase64String(sha256.ComputeHash(stream));
+                }
+            }
+        }
+    }
+}
